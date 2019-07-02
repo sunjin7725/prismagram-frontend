78378b5ef41a539df026db386d845760fbbe63a8
sunjin7725 <sunjin7725@gmail.com>
2019-07-02 21:33:33 +0900

    #3 Setting up Router page

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,9 +2,13 @@ import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import {ThemeProvider} from "styled-components";
 import Theme from "../Styles/Theme";
+import Router from "./Router";
 
 export default () => (
 <ThemeProvider theme={Theme}>
-    <GlobalStyles />
+    <>
+        <GlobalStyles />
+        <Router isLoggedIn={false}/>
+    </>
 </ThemeProvider>
 );
---
+++ src/Components/Router.js
@@ -0,0 +1,23 @@
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import PropTypes from "prop-types";
+import Feed from "../Routes/Feed";
+import Auth from "../Routes/Auth";
+import React from "react";
+
+const LoggedInRoutes = () => <> 
+    <Route exact path="/" component={Feed}/> 
+</>;
+
+const LoggedOutRoutes =  () => <> 
+    <Route exact path="/" component={Auth}/> 
+</>;
+
+const AppRouter = ({isLoggedIn}) =>(<Router>
+    <Switch>{isLoggedIn ? <LoggedInRoutes/>:<LoggedOutRoutes/>}</Switch>
+</Router>);
+
+AppRouter.propTypes = {
+    isLoggedIn : PropTypes.bool.isRequired
+};
+
+export default AppRouter;
